124cff2c704eb0521a361c0bd0eb109f4036015d
Lingcao Huang <huanglingcao@gmail.com>
2020-05-26 20:25:19 +0800

    rename

--- cp_to_dropbox.sh
+++ cp_to_dropbox.sh
@@ -2,7 +2,7 @@
 
 dir="/Users/huanglingcao/Dropbox/Research/Conference or written Paper/06 polygon-based change detection of thaw slumps"
 
-name=poygon_based_rts_changeDet
+name=polygon_based_rts_changeDet
 
 cp -p ${name}.pdf "${dir}"/.
 #cp -p ${name}.docx "${dir}"/.
