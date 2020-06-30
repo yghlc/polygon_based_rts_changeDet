e8f6dfa3773ca13edb14b9fe7c57ce87d75a77e8
Lingcao Huang <huanglingcao@gmail.com>
2020-06-30 20:21:04 +0800

    typo fix for bib file

--- updatebib.sh
+++ updatebib.sh
@@ -4,18 +4,25 @@
 
 project=polygon_based_rts_changeDet
 
+#cp /Users/huanglingcao/codes/Texpad/shared_files/permafrost_rs_ref.bib .
+
 # to get the aux file
 pdflatex ${project}.tex
 
+
 # replace \bibdata{} to the absulute path incase it cannot find it
-bibpath=/Users/huanglingcao/codes/Texpad/shared_files/thesis_ref
-sed -i .bak -e s%\bibdata{thesis_ref}%\bibdata{${bibpath}}%g ${project}.aux 
+bibpath=/Users/huanglingcao/codes/Texpad/shared_files/permafrost_rs_ref.bib
+sed -i .bak -e s%\bibdata{permafrost_rs_ref.bib}%\bibdata{${bibpath}}%g ${project}.aux
+
+# remove other label could cause errors
+cat ${project}.aux | grep \citation  > test.aux
+cat ${project}.aux | grep \bibdata >> test.aux
 
 # extract the sub-bibfile
-bibtool -x ${project}.aux > ${project}.bib
+bibtool -x test.aux > ${project}.bib
 
 # rm files
-rm ${project}.aux ${project}.aux.bak ${project}.log
+rm *.aux ${project}.aux.bak ${project}.log ${project}.out ${project}.spl
 
 
 
